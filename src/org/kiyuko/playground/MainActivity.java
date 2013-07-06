743546bac2c7e45a8fb0c1309ee968fdb8ea7400
Andrea Bolognani <eof@kiyuko.org>
2013-07-06 19:37:48 +0200

    Add some dummy list items.

--- src/org/kiyuko/playground/MainActivity.java
+++ src/org/kiyuko/playground/MainActivity.java
@@ -4,14 +4,27 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private String[] listItems = {
+			"Item one",
+			"Item two",
+			"Item three"
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listItems);
+
+		ListView listView1 = (ListView) findViewById(R.id.listView1);
+		listView1.setAdapter(adapter);
 	}
 
 	@Override
