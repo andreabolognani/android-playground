1534318be1486828e9d4e18e9df6ea11acf83776
Andrea Bolognani <eof@kiyuko.org>
2013-07-10 20:45:36 +0200

    Add buttons to the dialog.

--- src/org/kiyuko/playground/ItemAddDialogFragment.java
+++ src/org/kiyuko/playground/ItemAddDialogFragment.java
@@ -4,9 +4,12 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ItemAddDialogFragment extends DialogFragment {
 
@@ -23,6 +26,41 @@ public class ItemAddDialogFragment extends DialogFragment {
 		builder = new AlertDialog.Builder(getActivity());
 		builder.setTitle(R.string.itemAddDialogFragmentTitle);
 		builder.setView(view);
+
+		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				AlertDialog dial;
+				EditText nameEdit;
+				EditText descriptionEdit;
+				Toast toast;
+				String message;
+				int duration;
+
+				dial = (AlertDialog) dialog;
+				nameEdit = (EditText) dial.findViewById(R.id.nameEdit);
+				descriptionEdit = (EditText) dial.findViewById(R.id.descriptionEdit);
+
+				message = "(\"";
+				message += nameEdit.getText().toString();
+				message += "\" \"";
+				message += descriptionEdit.getText().toString();
+				message += "\")";
+				duration = Toast.LENGTH_SHORT;
+
+				toast = Toast.makeText(dial.getContext(), message, duration);
+				toast.show();
+			}
+		});
+
+		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
 
 		return builder.create();
 	}
